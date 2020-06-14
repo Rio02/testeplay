538fdaf973f7d8d8213f90b0aa11ba14cfb7908a
Vitor lohan <civilperdido06@gmail.com>
2020-06-14 18:49:02 -0300

    commit

--- server/src/server.ts
+++ server/src/server.ts
@@ -8,7 +8,7 @@ app.use(cors());
 app.use(express.json());
 app.use(rotas);
 
-const PORT = process.env.PORT || 3333;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log('Escutando porta ' + PORT);
 })
