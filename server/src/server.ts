8514fb7c445535317229225de72e3b514f384f30
Vitor lohan <civilperdido06@gmail.com>
2020-06-14 18:42:30 -0300

    teste porta

--- server/src/server.ts
+++ server/src/server.ts
@@ -8,4 +8,7 @@ app.use(cors());
 app.use(express.json());
 app.use(rotas);
 
-app.listen(process.env.PORT || 3333);
+const PORT = process.env.PORT || 3333;
+app.listen(PORT, () => {
+    console.log('Escutando porta ' + PORT);
+})
